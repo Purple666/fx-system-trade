bd9fa09d0592327e15c6138a1c96c2cd9e6e86a8
MIYATA Tadaaki <andes@flg.jp>
2018-08-10 22:19:01 +0900

    Experiment

--- fx-system-trade/src/GlobalSettingFunction.hs
+++ fx-system-trade/src/GlobalSettingFunction.hs
@@ -9,11 +9,11 @@ import qualified FxSetting                as Fs
 
 getEvaluationValue :: Ftd.FxTradeData -> Double
 getEvaluationValue x =
+  Ftd.profit x
+{-  
   if Ftd.unrealizedPL x <= Gsd.initalProperty Gsd.gsd
   then 0
   else Ftd.profit x * (Ftd.unrealizedPL x - Gsd.initalProperty Gsd.gsd) * Ftd.getWinRatePure x
-{-  
-  Ftd.profit x
 
   if Ftd.trTrade x == 0 || Ftd.trTradeDate x == 0
   then 0
