b87207ec8bd0d30b2e5228038151dd0b959f0e5b
MIYATA Tadaaki <andes@flg.jp>
2018-09-01 17:07:12 +0900

    Experiment

--- fx-system-trade/src/FxSetting.hs
+++ fx-system-trade/src/FxSetting.hs
@@ -49,7 +49,7 @@ getLearningTestTime fsd =
 
 getLearningTestTimes :: Fsd.FxSettingData -> Double
 getLearningTestTimes fsd =
-  (sqrt :: (Double -> Double)) . fromIntegral .  Fsd.learningTestTimes $ Fsd.learningSetting fsd
+  (log :: (Double -> Double)) $ (fromIntegral .  Fsd.learningTestTimes $ Fsd.learningSetting fsd) + 2
   --
 
 getTradeHoldTime :: Fsd.FxSettingData -> Int
