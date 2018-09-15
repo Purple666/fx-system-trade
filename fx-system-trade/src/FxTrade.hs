62dfa8e9ffa97ca4070e1d7d7d4ca0eb3e25db33
MIYATA Tadaaki <andes@flg.jp>
2018-09-15 10:50:30 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -68,16 +68,16 @@ evaluate ctd fsd f1 forceSell td =
   let
 {-    
   (position, open)
-    | (Ftd.side td == Ftd.None || Ftd.side td == Ftd.Sell) &&
+    | Ftd.side td == Ftd.None &&
       evaluateProfitInc fto ftado = (chart, Ftd.Buy)
-    | (Ftd.side td == Ftd.None || Ftd.side td == Ftd.Buy) &&
+    | Ftd.side td == Ftd.None &&
       evaluateProfitDec fto ftado = (chart, Ftd.Sell)
     | otherwise = (0, Ftd.None)
 -}      
   (position, open)
-    | Ftd.side td == Ftd.None &&
+    | (Ftd.side td == Ftd.None || (Fs.getTradeHoldTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) && Ftd.side td == Ftd.Sell)) &&
       evaluateProfitInc fto ftado = (chart, Ftd.Buy)
-    | Ftd.side td == Ftd.None &&
+    | (Ftd.side td == Ftd.None || (Fs.getTradeHoldTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) && Ftd.side td == Ftd.Buy)) &&
       evaluateProfitDec fto ftado = (chart, Ftd.Sell)
     | otherwise = (0, Ftd.None)
   (profits, realizedPL, close)
