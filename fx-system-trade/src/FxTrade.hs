814b73029f920906b82ed3694be0870c6cd520e7
MIYATA Tadaaki <andes@flg.jp>
2018-09-16 23:10:01 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -103,14 +103,14 @@ evaluate ctd fsd f1 forceSell td =
                                            then (rate - chart, 1 - (chart / rate), Ftd.Close)
                                            else (0, 0, Ftd.None)
     | rate /= 0 = if Ftd.side td == Ftd.Buy &&
-                     (forceSell || Fs.getLearningTestTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) ||
+                     (forceSell || Fs.getLearningTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) ||
                      (Fs.getTradeHoldTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) &&
                       (0 < chart - rate && evaluateProfitDec ftcp ftadcp ||
                        chart - rate < 0 && evaluateProfitDec ftcl ftadcl ||
                        chart - rate < Fs.getLossCutRate fsd)))
                   then (chart - rate, (chart / rate) - 1, Ftd.Buy)
                   else if Ftd.side td == Ftd.Sell &&
-                          (forceSell || Fs.getLearningTestTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) ||
+                          (forceSell || Fs.getLearningime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) ||
                           (Fs.getTradeHoldTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) &&
                            (0 < rate - chart && evaluateProfitInc ftcp ftadcp ||
                             rate - chart < 0 && evaluateProfitInc ftcl ftadcl ||
