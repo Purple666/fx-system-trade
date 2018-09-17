f964a330a29d72352080cf4310d4aea63942d1e8
MIYATA Tadaaki <andes@flg.jp>
2018-09-17 12:51:50 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -102,7 +102,7 @@ learningLoop c cl ce fsd fsds = do
   if Ft.evaluationOk tdl tdlt
     then return (0, True, tdl, tdlt, fsd')
     else if fsd == fsd' && 0 < Ft.getProfitList tdlt
-         then return (0, False, tdl, tdlt, fsd')
+         then return (0, False, tdl, tdlt, Fsd.plusLearningTestTimes fsd')
          else if Fs.getLearningTestTimes fsd' < fromIntegral c
               then return (0, False, tdl, tdlt, Fsd.plusLearningTestTimes fsd')
               else learningLoop (c + 1) cl ce fsd' $ map (\(_, _, _, x) -> x) fsds'
