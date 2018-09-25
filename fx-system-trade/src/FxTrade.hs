f9a21608919525774a0cdabeee64a344d163a274
MIYATA Tadaaki <andes@flg.jp>
2018-09-25 13:51:41 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -94,13 +94,15 @@ evaluate ctd fsd f1 forceSell td =
     | Ftd.side td == Ftd.None &&
       evaluateProfitDec fto ftado = (chart, Ftd.Sell)
     | otherwise = (0, Ftd.None)
--}      
+
   (position, open)
     | (Ftd.side td == Ftd.None || (Fs.getTradeHoldTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) && Ftd.side td == Ftd.Sell)) &&
       evaluateProfitInc fto ftado = (chart, Ftd.Buy)
     | (Ftd.side td == Ftd.None || (Fs.getTradeHoldTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) && Ftd.side td == Ftd.Buy)) &&
       evaluateProfitDec fto ftado = (chart, Ftd.Sell)
     | otherwise = (0, Ftd.None)
+-}
+  (position, open)                                                                                                           | Ftd.side td == Ftd.None &&                                                                                               evaluateProfitInc fto ftado = (chart, Ftd.Buy)                                                                         | Ftd.side td == Ftd.None &&                                                                                               evaluateProfitDec fto ftado = (chart, Ftd.Sell)                                                                        | otherwise = (0, Ftd.None) 
   (profits, realizedPL, close)
     | open /= Ftd.None && rate /= 0 = if Ftd.side td == Ftd.Buy
                                       then (chart - rate, (chart / rate) - 1, Ftd.Close)
