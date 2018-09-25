dc160cfe4f84229dc539d7bfbc3c1edfae10b452
MIYATA Tadaaki <andes@flg.jp>
2018-09-25 14:42:49 +0900

    Experiment

--- fx-system-trade/src/GlobalSettingData.hs
+++ fx-system-trade/src/GlobalSettingData.hs
@@ -20,16 +20,16 @@ data GlobalSettingData = GlobalSettingData
 
 gsd :: GlobalSettingData
 gsd =
-  GlobalSettingData { taMargin              = 5
-                    , makeTreeCount         = 5
-                    , algorithmRepeat       = 5
-                    , countUpListMax        = 5
-                    , learningTestCount     = 5
+  GlobalSettingData { taMargin              = 4
+                    , makeTreeCount         = 4
+                    , algorithmRepeat       = 4
+                    , countUpListMax        = 4
+                    , learningTestCount     = 4
                     , quantityRate          = 5
-                    , thresholdRate         = 0.05
+                    , thresholdRate         = 0.1
                     , initalProperty        = 500000
                     , maxUnit               = 3000000
-                    , spread                = 0.004
+                    , spread                = 0.000
                     , dbHost                = "mongo:27017"
                     , tradePracticeBearer   = "Bearer b8831e8a4c0974fc5207eb9c4d844845-96edf86dff693710db11e5a333c18117"
                     , tradePracticeUrl      = "https://api-fxpractice.oanda.com/v1/accounts/6716490"
