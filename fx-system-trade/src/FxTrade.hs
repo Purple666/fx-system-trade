d5e28336e874965995edd3ad6840085fc12ab0bd
MIYATA Tadaaki <andes@flg.jp>
2018-09-28 17:06:45 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -99,6 +99,7 @@ evaluate ctd fsd f1 forceSell td =
       evaluateProfitDec fto ftado = (chart, Ftd.Sell)
     | otherwise = (0, Ftd.None)
 -}
+  
   let (position, open)
         | (Ftd.side td == Ftd.None || (0 < rate - chart && Fs.getTradeHoldTime fsd < Fcd.no cd - Fcd.no (Ftd.rate td) && Ftd.side td == Ftd.Sell)) &&
           evaluateProfitInc fto ftado = (chart, Ftd.Buy)
