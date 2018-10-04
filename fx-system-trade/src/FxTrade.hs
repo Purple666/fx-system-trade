731cc6c80551668364bd1375dfa42ccc2f2b5de2
MIYATA Tadaaki <andes@flg.jp>
2018-10-04 17:13:53 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -137,16 +137,16 @@ evaluate ctd fsd f1 forceSell td =
                               (forceSell || Fs.getLearningTestTime fsd < Fcd.no cd - tradeNo ||
                                (Fs.getTradeHoldTime fsd < Fcd.no cd - tradeNo &&
                                 ((0 < chart - tradeRate && evaluateProfitDec ftcp ftadcp) ||
-                                 (chart - tradeRate < 0 && evaluateProfitDec ftcl ftadcl)) ||
-                                 {- Fs.getProfitRate fsd < unrealizedPL - Ftd.realizedPL td ||
+                                 (chart - tradeRate < 0 && evaluateProfitDec ftcl ftadcl)) {- ||
+                                 Fs.getProfitRate fsd < unrealizedPL - Ftd.realizedPL td ||
                                  unrealizedPL - Ftd.realizedPL td < Fs.getLossCutRate fsd -}))
                            then (chart - tradeRate, (chart / tradeRate) - 1, Ftd.Buy)
                            else if Ftd.side td == Ftd.Sell &&
                                    (forceSell || Fs.getLearningTestTime fsd < Fcd.no cd - tradeNo ||
                                     (Fs.getTradeHoldTime fsd < Fcd.no cd - tradeNo &&
                                      ((0 < tradeRate - chart && evaluateProfitInc ftcp ftadcp) ||
-                                      (tradeRate - chart < 0 && evaluateProfitInc ftcl ftadcl)) || 
-                                      {- Fs.getProfitRate fsd < unrealizedPL - Ftd.realizedPL td ||
+                                      (tradeRate - chart < 0 && evaluateProfitInc ftcl ftadcl)) {- || 
+                                      Fs.getProfitRate fsd < unrealizedPL - Ftd.realizedPL td ||
                                       unrealizedPL - Ftd.realizedPL td < Fs.getLossCutRate fsd -}))
                                 then (tradeRate - chart, 1 - (chart / tradeRate), Ftd.Sell)
                                 else (0, 0, Ftd.None)
