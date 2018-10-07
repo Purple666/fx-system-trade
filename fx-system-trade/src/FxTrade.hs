12ba6a1fe7cb8a4600b6a242189d65cfd7cb97db
MIYATA Tadaaki <andes@flg.jp>
2018-10-07 15:56:33 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -43,7 +43,8 @@ getQuantityBacktest td chart = Gsd.initalProperty Gsd.gsd / Gsd.quantityRate Gsd
 -}
 
 getQuantityLearning :: Ftd.FxTradeData -> Double -> Double
-getQuantityLearning td chart = Gsd.initalProperty Gsd.gsd / Gsd.quantityRate Gsd.gsd -- Ftd.realizedPL td / Gsd.quantityRate Gsd.gsd -- Gsd.initalProperty Gsd.gsd
+getQuantityLearning td chart = Ftd.realizedPL td
+-- Gsd.initalProperty Gsd.gsd / Gsd.quantityRate Gsd.gsd 
 
 evaluateProfitInc :: Fad.FxTechnicalAnalysisSetting -> M.Map Int Fad.FxTechnicalAnalysisData -> Bool
 evaluateProfitInc fts ftad =
