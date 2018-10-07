b545ef1e98cfc878628c4ae755d976a7f7c3302a
MIYATA Tadaaki <andes@flg.jp>
2018-10-07 15:29:47 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -163,7 +163,7 @@ backTestLoop s latest n endN td fsd = do
   Fp.printTestProgress (Fcd.date $ Ftd.chart td) (Fcd.date $ Ftd.chart tdt) fsd tdt tdl tdlt plsf lsf
   if endN <= n' || Ftd.realizedPL tdt < Gsd.initalProperty Gsd.gsd / Gsd.quantityRate Gsd.gsd
     then return (Gsd.initalProperty Gsd.gsd < Ftd.realizedPL tdt, fsd2)
-    else backTestLoop (Ftd.profit td <= Ftd.profit tdt) latest n' endN tdt fsd2
+    else backTestLoop (Ftd.profit td < Ftd.profit tdt) latest n' endN tdt fsd2
 
 tradeEvaluate :: Ftd.FxTradeData ->
                  Fsd.FxSettingData ->
