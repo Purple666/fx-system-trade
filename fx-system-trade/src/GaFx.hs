1ab43740db1e71866c30c8fd08d81339579ddd5a
MIYATA Tadaaki <andes@flg.jp>
2018-10-07 13:44:27 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -155,7 +155,7 @@ backTestLoop latest retry failp n endN td fsd = do
   (plsf, lsf, tdl, tdlt, fsd1) <- learning failp (n - 1) fsd
   let lt  = Fs.getLearningTime     fsd1
       ltt = Fs.getLearningTestTime fsd1
-  (tdt, fsd2) <- Ft.backTest latest endN (lt + ltt * Gsd.learningTestCount Gsd.gsd) td fsd1
+  (tdt, fsd2) <- Ft.backTest latest endN (lt + ltt * Gsd.learningTestCount Gsd.gsd) (Ftd.trFail tdl + (sum $ map Ftd.trFail tdlt)) td fsd1
                  =<< ((++) <$>
                        Fm.getChartListBack    (n - 1) (Fs.getPrepareTimeAll fsd1) 0 <*>
                        Fm.getChartListForward n       (lt + ltt * Gsd.learningTestCount Gsd.gsd) 0)
