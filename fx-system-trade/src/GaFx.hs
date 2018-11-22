edff4c52a377a8a2cb7ab5124bb21206fc776161
MIYATA Tadaaki <andes@flg.jp>
2018-11-23 08:46:45 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -242,8 +242,8 @@ tradeLoop p sleep td fsd coName a = do
 -}
   td2 <- tradeEvaluate td fsd' coName =<<
          ((++) <$> Fm.getChartListBack (Fcd.no e - 1) (Fs.getPrepareTimeAll fsd') 0 <*> pure [e])
-  traceShow(td2) $ return ()
-  tradeLoop e sleep td2 fsd' coName a'
+  
+  Ftd.unrealizedPL td2 `seq` tradeLoop e sleep td2 fsd' coName a'
 {-
   if 240 < sleep'
     then do cancel a'
