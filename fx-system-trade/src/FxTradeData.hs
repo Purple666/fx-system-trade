165d75248c752e43c08553b17677eb9b0490b70a
MIYATA Tadaaki <andes@flg.jp>
2018-11-23 10:44:26 +0900

    Experiment

--- fx-system-trade/src/FxTradeData.hs
+++ fx-system-trade/src/FxTradeData.hs
@@ -14,7 +14,7 @@ import qualified FxTechnicalAnalysisData as Fad
 import qualified GlobalSettingData       as Gsd
 
 data FxTradeData =
-  FxTradeData { chart              :: !Fcd.FxChartData
+  FxTradeData { chart              :: Fcd.FxChartData
               , tradeRate          :: Fcd.FxChartData
               , alcOpen            :: Fad.FxalgorithmListCount
               , alcCloseProfit     :: Fad.FxalgorithmListCount
@@ -28,7 +28,7 @@ data FxTradeData =
               , trFail             :: Int
               , profit             :: Double
               , unrealizedPL       :: Double
-              , realizedPL         :: Double
+              , realizedPL         :: !Double
               , environment        :: FxEnvironment
               , bearer             :: String
               , url                :: String
