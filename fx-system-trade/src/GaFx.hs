5c511d202779b9110fdf7afdd982fbf09afe1fe4
MIYATA Tadaaki <andes@flg.jp>
2018-12-05 15:37:44 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -223,7 +223,7 @@ checkTradeLearning a fsd = do
   case e of
     Nothing -> return (a, fsd)
     Just _  -> do fsd' <- wait a
-                  a' <- async $ tradeLearningThread fsd'
+                  a' <- async $ tradeLearningThread fsd
                   return (a', fsd')
 
 tradeLoop :: Fcd.FxChartData ->
@@ -236,8 +236,8 @@ tradeLoop :: Fcd.FxChartData ->
 tradeLoop p sleep td fsd coName a = do
   -- t <- getCurrentTime
   -- threadDelay ((15 - (truncate (utcTimeToPOSIXSeconds t) `mod` 15)) * 1000 * 1000)
-  -- (a', fsd') <- checkTradeLearning a fsd
-  (a', fsd') <- return (a, fsd)
+  (a', fsd') <- checkTradeLearning a fsd
+  -- (a', fsd') <- return (a, fsd)
   e <- Foa.getNowPrices td
   (sleep', td2) <- if e /= p
                    then do td1 <- tradeEvaluate td fsd' coName =<<
