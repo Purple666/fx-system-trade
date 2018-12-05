3bd2f38012384c1ea23a4a6728274d578a6556b7
MIYATA Tadaaki <andes@flg.jp>
2018-12-05 20:58:24 +0900

    Experiment

--- fx-system-trade/src/FxTradeData.hs
+++ fx-system-trade/src/FxTradeData.hs
@@ -16,9 +16,9 @@ import qualified GlobalSettingData       as Gsd
 data FxTradeData =
   FxTradeData { chart              :: Fcd.FxChartData
               , tradeRate          :: Fcd.FxChartData
-              , alcOpen            :: Fad.FxalgorithmListCount
-              , alcCloseProfit     :: Fad.FxalgorithmListCount
-              , alcCloseLoss       :: Fad.FxalgorithmListCount
+              , alcOpen            :: !Fad.FxalgorithmListCount
+              , alcCloseProfit     :: !Fad.FxalgorithmListCount
+              , alcCloseLoss       :: !Fad.FxalgorithmListCount
               , side               :: FxSide
               , trTradeDate        :: Int
               , trTrade            :: Int
