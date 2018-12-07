bcdba4eaca8dcac1c12b357baf34ef102e5ba151
MIYATA Tadaaki <andes@flg.jp>
2018-12-07 10:28:34 +0900

    Experiment

--- fx-system-trade/src/FxSetting.hs
+++ fx-system-trade/src/FxSetting.hs
@@ -124,7 +124,7 @@ updateFxSettingLog fsd fss =
 
 updateFxSettingLog2 :: M.Map Fsd.FxSetting (Double, Int) -> M.Map Fsd.FxSetting (Double, Int)
 updateFxSettingLog2 fsl =
-  M.withoutKeys fsl . fromList . map (\(x, (_, _)) -> x) . take (length fsl - Gsd.fxSettingLogNum Gsd.gsd) .
+  M.withoutKeys fsl . S.fromList . map (\(x, (_, _)) -> x) . take (length fsl - Gsd.fxSettingLogNum Gsd.gsd) .
   sortBy (\(_, (a, _)) (_, (b, _)) -> compare a b) $ M.toList fsl
 
 updateFxSettingData :: [Fad.FxChartTaData] -> Ftd.FxTradeData -> Ftd.FxTradeData -> Fsd.FxSettingData -> Fsd.FxSettingData -> Fsd.FxSettingData
