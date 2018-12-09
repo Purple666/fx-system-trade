470938201f7a4baed8a7c77d49eb28880c4c3fe3
MIYATA Tadaaki <andes@flg.jp>
2018-12-09 12:36:05 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -364,8 +364,11 @@ trade :: Ftd.FxTradeData ->
          (Ftd.FxSide, Ftd.FxSide, Ftd.FxTradeData)
 trade td fsd xcd =
   let ctdl = makeChart fsd 1 xcd
-      (open, close, td') =  evaluate False (last ctdl) fsd getQuantityLearning False td  
+  in evaluate False (last ctdl) fsd getQuantityLearning False td
+{-   
+      (open, close, td') =  
   in (open, close, resetCounter td')
+-}
 
 gaLearningEvaluate :: Fsd.FxSettingData -> (Fsd.FxSettingData, Rational)
 gaLearningEvaluate fsd =
