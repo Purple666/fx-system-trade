b326ed1289aebb89f436f97e8bccf9adce94b0c5
MIYATA Tadaaki <andes@flg.jp>
2018-12-12 20:27:24 +0900

    Experiment

--- fx-system-trade/src/FxPrint.hs
+++ fx-system-trade/src/FxPrint.hs
@@ -49,7 +49,7 @@ printLearningFxTradeData p n fsd tdl tdlt plsf lsf = do
 printProgressFxTradeData :: Ftd.FxTradeData -> Fcd.FxChartData -> IO ()
 printProgressFxTradeData td e = do
   printf "%s " =<< Ftm.getLogTime
-  printf "%7.3f %7.3f %8.3f" (Fcd.close e) (Fcd.close e $ Ftd.tradeRate td) ((Fcd.close e) - (Fcd.close e $ Ftd.tradeRate td))
+  printf "%7.3f %7.3f %8.3f" (Fcd.close e) (Fcd.close $ Ftd.tradeRate td) ((Fcd.close e) - (Fcd.close $ Ftd.tradeRate td))
   printFxTradeData td
   printf "\n"
 
