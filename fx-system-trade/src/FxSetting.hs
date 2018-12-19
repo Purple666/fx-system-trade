e3aaac0895af8c84648472065037995ac600ab6a
MIYATA Tadaaki <andes@flg.jp>
2018-12-19 16:58:07 +0900

    Experiment

--- fx-system-trade/src/FxSetting.hs
+++ fx-system-trade/src/FxSetting.hs
@@ -22,6 +22,7 @@ module FxSetting
 import           Control.Monad
 import           Control.Monad.Random
 import           Data.List
+import           Data.List.Unique 
 import qualified Data.Map                as M
 import qualified Data.Set                as S
 import           Debug.Trace
@@ -121,7 +122,7 @@ updateFxSettingLog plsf fsl =
            c = M.restrictKeys fsl a
            b = M.withoutKeys fsl a
            d = S.fromList  . map (\(x, (_, _)) -> x) $ M.toList fsl
-           g = S.fromList $ M.keys fsl
+           g = S.fromList . unique $ M.keys fsl
            e = M.withoutKeys fsl g
            f = M.restrictKeys fsl g
        in traceShow(length fsl, length a, length b, length c, length g, length e, length f) $ b 
