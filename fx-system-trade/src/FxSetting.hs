41f67f9ab48f4f364b62115d832e803808caa0fe
MIYATA Tadaaki <andes@flg.jp>
2018-12-26 11:14:31 +0900

    Experiment

--- fx-system-trade/src/FxSetting.hs
+++ fx-system-trade/src/FxSetting.hs
@@ -132,8 +132,8 @@ updateFxSettingData :: [Fad.FxChartTaData] ->
 updateFxSettingData ctdl plsf td tdt acc fsd fsdo =
   let p = Ftd.profit tdt - Ftd.profit td
       lso = Fsd.learningSetting $ Fsd.fxSetting fsdo
-      ls  = Fsd.learningSetting . unionWith (\(a, b) (a', b') -> (max a a', max b b')) (Fsd.fxSetting fsd) (Fsd.fxSetting fsdo)
-      fslu = updateFxSettingLog plsf (Fsd.fxSettingLog fsdo)
+      ls  = Fsd.learningSetting $ Fsd.fxSetting fsd
+      fslu = updateFxSettingLog plsf $  M.unionWith (\(a, b) (a', b') -> (max a a', max b b')) (Fsd.fxSetting fsd) (Fsd.fxSetting fsdo)
       fsl = M.filter (\(pp, _) -> 0 < pp) $ if M.member (Fsd.fxSetting fsd) fslu
                                             then M.adjust (\(a, b) -> (a + p, b + 1)) (Fsd.fxSetting fsd) fslu
                                             else M.insert (Fsd.fxSetting fsd) (p, 1) fslu
