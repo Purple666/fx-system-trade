6159f069403de68f6e4b1b662cd43f6604233a22
MIYATA Tadaaki <andes@flg.jp>
2018-12-30 22:15:22 +0900

    Experiment

--- fx-system-trade/src/GlobalSettingData.hs
+++ fx-system-trade/src/GlobalSettingData.hs
@@ -23,11 +23,11 @@ data GlobalSettingData = GlobalSettingData
 
 gsd :: GlobalSettingData
 gsd =
-  GlobalSettingData { taMargin              = 5
-                    , makeTreeCount         = 5
-                    , algorithmRepeat       = 5
-                    , countUpListMax        = 5
-                    , learningTestCount     = 3
+  GlobalSettingData { taMargin              = 10
+                    , makeTreeCount         = 10
+                    , algorithmRepeat       = 10
+                    , countUpListMax        = 10
+                    , learningTestCount     = 10
                     , taAndRate             = 2
                     , quantityRate          = 5
                     , thresholdRate         = 0.01
