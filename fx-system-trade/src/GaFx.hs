05c70802cec31e463851a8e710fb964c5bc5fbf1
MIYATA Tadaaki <andes@flg.jp>
2019-01-05 15:10:06 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -103,7 +103,7 @@ learningLoop c n fsd = do
   -- Fp.printLearningFxTradeData p 0 lt ltt fsd' tdl tdlt 0 (Ft.evaluationOk tdl tdlt) (fsd == fsd')
   if Ft.evaluationOk tdl tdlt
     then return (0, True, tdl, tdlt, Fsd.setNo n fsd')
-    else if Fs.getLearningTestTimes fsd' < fromIntegral c || (fsd == fsd' && Ft.evaluationOk2 tdl tdlt)
+    else if Fs.getLearningTestTimes fsd' < fromIntegral c || fsd == fsd' -- && Ft.evaluationOk2 tdl tdlt)
          then return (0, False, tdl, tdlt, Fsd.setNo n $ Fsd.plusLearningTestTimes fsd')
          else learningLoop (c + 1) n fsd' 
 
