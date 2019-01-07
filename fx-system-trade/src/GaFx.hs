f87033c54c025c5641c9210c05f00d4ef7176ca3
MIYATA Tadaaki <andes@flg.jp>
2019-01-07 14:59:45 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -105,9 +105,11 @@ learningLoop c n p fsd = do
   Fp.printLearningFxTradeData p' 0 lt ltt fsd' tdl tdlt 0 (Ft.evaluationOk tdl tdlt) (fsd == fsd')
   if Ft.evaluationOk tdl tdlt
     then return (0, True, tdl, tdlt, Fsd.setNo n fsd')
-    else if (Fsd.learningTestTimes . Fsd.learningSetting $ Fsd.fxSetting fsd') < fromIntegral c
-         then return (0, False, tdl, tdlt, Fsd.setNo n $ Fsd.plusLearningTestTimes fsd')
-         else learningLoop (c + 1) n p' fsd' 
+    else if Ft.evaluationOk2 tdl tdlt && p == p'
+         then return (0, False, tdl, tdlt, Fsd.setNo n fsd')
+         else if (Fsd.learningTestTimes . Fsd.learningSetting $ Fsd.fxSetting fsd') < fromIntegral c
+              then return (0, False, tdl, tdlt, Fsd.setNo n $ Fsd.plusLearningTestTimes fsd')
+              else learningLoop (c + 1) n p' fsd' 
 
 learning :: Int ->
             Fsd.FxSettingData ->
