c0035a68f18f608f2433d8adfb1bf2434423d65f
MIYATA Tadaaki <andes@flg.jp>
2019-01-12 14:25:31 +0900

    Experiment

--- fx-system-trade/src/GaFx.hs
+++ fx-system-trade/src/GaFx.hs
@@ -96,13 +96,13 @@ learningLoop c n p fsd = do
                               tdl  = Ft.learning (Ft.initFxTradeData Ftd.Backtest) $ Fsd.nextFxSettingData lt' cl' x
                               p    = 10000000 * (Ftd.getEvaluationValue tdl + Ftd.getEvaluationValueList tdlt) /
                                      fromIntegral (lt' + ltt' * Gsd.learningTestCount Gsd.gsd)
-                          return (p, tdl, tdlt, x)) . (fsd:) .  Ga.getGaDataList) <$>
+                          return (p, tdl, tdlt, x)) . {- (fsd:) . -} Ga.getGaDataList) <$>
            (Ga.learning . Ga.learningData $ Fsd.nextFxSettingData lt cl fsd)
   fsds'' <- sequence $ fsds'
   let (p', tdl, tdlt, fsd') = maximum fsds''
       lt   = Fs.getLearningTime     fsd'
       ltt  = Fs.getLearningTestTime fsd'
-  -- Fp.printLearningFxTradeData p' 0 lt ltt fsd' tdl tdlt 0 (Ft.evaluationOk tdl tdlt) (fsd == fsd')
+  Fp.printLearningFxTradeData p' 0 lt ltt fsd' tdl tdlt 0 (Ft.evaluationOk tdl tdlt) (fsd == fsd')
   if Ft.evaluationOk tdl tdlt
     then return (0, True, tdl, tdlt, Fsd.setNo n fsd')
     else if {- Ft.evaluationOk2 tdl tdlt && -} p == p'
