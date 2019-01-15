241420e0ad3b2b6b65ba020dc5d50d529a74a3a7
MIYATA Tadaaki <andes@flg.jp>
2019-01-15 19:56:42 +0900

    Experiment

--- fx-system-trade/src/GlobalSettingData.hs
+++ fx-system-trade/src/GlobalSettingData.hs
@@ -23,10 +23,10 @@ data GlobalSettingData = GlobalSettingData
 
 gsd :: GlobalSettingData
 gsd =
-  GlobalSettingData { taMargin              = 10
-                    , algorithmRepeat       = 10
-                    , makeTreeCount         = 10
-                    , learningTestCount     = 10
+  GlobalSettingData { taMargin              = 5
+                    , algorithmRepeat       = 5
+                    , makeTreeCount         = 5
+                    , learningTestCount     = 5
                     , taAndRate             = 2
                     , countUpList           = 1.5
                     , countUpListMax        = 2
@@ -42,5 +42,6 @@ gsd =
                     , tradeProductionBearer = "Bearer b8831e8a4c0974fc5207eb9c4d844845-96edf86dff693710db11e5a333c18117"
                     , tradeProductionUrl    = "https://api-fxpractice.oanda.com/v1/accounts/6716490"
                     }
+
 
 
