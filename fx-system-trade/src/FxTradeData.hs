786f1b395ce8442106c1f0ad03683c5268bd157f
MIYATA Tadaaki <andes@flg.jp>
2019-01-21 14:00:06 +0900

    Experiment

--- fx-system-trade/src/FxTradeData.hs
+++ fx-system-trade/src/FxTradeData.hs
@@ -81,7 +81,7 @@ initFxTradeDataCommon =
 
 getEvaluationValue :: FxTradeData -> Double
 getEvaluationValue x =
-  profit x * unrealizedPL x / (fromIntegral $ chartLength x)
+  profit x * unrealizedPL x * (getWinRatePure x) ^ (2 :: Int) / ((fromIntegral $ chartLength x) * tradeDateAve x)
   -- (profit x * unrealizedPL x * (getWinRatePure x) ^ (2 :: Int)) / ((fromIntegral $ chartLength x) * tradeDateAve x)
   
   --(unrealizedPL x) / (fromIntegral $ chartLength x)
