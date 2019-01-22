ab8a0d095045ca039a8ae4ff40a3f6729e201d7e
MIYATA Tadaaki <andes@flg.jp>
2019-01-23 08:53:36 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -148,12 +148,12 @@ evaluate ctd fsdi fsd f1 forceSell td =
 -}
         | Ftd.side td == Ftd.Buy && (forceSell || 
                                      (Fs.getTradeHoldTime fsdi < Fcd.no cd - tradeNo &&
-                                      (0 < chart - tradeRate && evaluateProfitDec ftcp ftadcp ||
-                                       chart - tradeRate < 0 && evaluateProfitDec ftcl ftadcl))) = (chart - tradeRate, Ftd.Buy)
+                                      0 < chart - tradeRate && evaluateProfitDec ftcp ftadcp) ||
+                                       chart - tradeRate < 0 && evaluateProfitDec ftcl ftadcl) = (chart - tradeRate, Ftd.Buy)
         | Ftd.side td == Ftd.Sell && (forceSell || 
                                       (Fs.getTradeHoldTime fsdi < Fcd.no cd - tradeNo &&
-                                       (0 < tradeRate - chart && evaluateProfitInc ftcp ftadcp ||
-                                        tradeRate - chart < 0 && evaluateProfitInc ftcl ftadcl))) = (tradeRate - chart, Ftd.Sell)
+                                       0 < tradeRate - chart && evaluateProfitInc ftcp ftadcp) ||
+                                        tradeRate - chart < 0 && evaluateProfitInc ftcl ftadcl) = (tradeRate - chart, Ftd.Sell)
         | otherwise = (0, Ftd.None)
       fsd' = if close /= Ftd.None
              then let ls  = Fsd.learningSetting $ Fsd.fxSetting fsd
