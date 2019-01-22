cac33f7838f18768bbfa7dd31a874d2254986417
MIYATA Tadaaki <andes@flg.jp>
2019-01-23 07:26:29 +0900

    Experiment

--- fx-system-trade/src/FxTradeData.hs
+++ fx-system-trade/src/FxTradeData.hs
@@ -81,7 +81,7 @@ initFxTradeDataCommon =
 
 getEvaluationValue :: FxTradeData -> Double
 getEvaluationValue x =
-  profit x * unrealizedPL x / ((fromIntegral $ chartLength x) * tradeDateAve x * getWinRatePure x)
+  profit x * unrealizedPL x * getWinRatePure x / ((fromIntegral $ chartLength x) * tradeDateAve x)
 
 getEvaluationValueList :: [FxTradeData] -> Double
 getEvaluationValueList tdlt =
