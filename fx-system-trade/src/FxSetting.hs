a3c130f20c1f348f05704f0dec1fb380286c8c23
MIYATA Tadaaki <andes@flg.jp>
2019-01-23 13:29:59 +0900

    Experiment

--- fx-system-trade/src/FxSetting.hs
+++ fx-system-trade/src/FxSetting.hs
@@ -51,7 +51,7 @@ getProfitRate fsd =
 getLearningTime :: Fsd.FxSettingData -> Int
 getLearningTime fsd =
   let ls = Fsd.learningSetting $ Fsd.fxSetting fsd
-  in truncate $ getLearningTestTimes fsd *
+  in truncate $ (fromIntegral . Fsd.learningTestTimes . Fsd.learningSetting $ Fsd.fxSetting fsd) *
      (if Fsd.trTrade ls == 0
       then 60
       else (fromIntegral $ Fsd.trTradeDate ls `div` Fsd.trTrade ls))
