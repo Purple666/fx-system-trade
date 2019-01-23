73af0c54c0073e87b1e32ff6c01875200ab25cbc
MIYATA Tadaaki <andes@flg.jp>
2019-01-23 14:46:25 +0900

    Experiment

--- fx-system-trade/src/FxTradeData.hs
+++ fx-system-trade/src/FxTradeData.hs
@@ -98,9 +98,9 @@ getEvaluationValue x =
 
 getLearningEvaluationValue :: FxTradeData -> Double
 getLearningEvaluationValue x =
-  if getWinRatePure x == 0
+  if getWinRatePure x == 0 || tradeDateAve x == 0
   then 0
-  else profit x / getWinRatePure x ^ 4
+  else (profit x * unrealizedPL x)  / (getWinRatePure x ^ 4 * tradeDateAve x)
 
 getEvaluationValueList :: [FxTradeData] -> Double
 getEvaluationValueList tdlt =
