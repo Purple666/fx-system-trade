afd6390aa4af913561102a08808ad8f71ed4ae83
MIYATA Tadaaki <andes@flg.jp>
2019-01-27 16:06:30 +0900

    Experiment

--- fx-system-trade/src/FxSetting.hs
+++ fx-system-trade/src/FxSetting.hs
@@ -139,11 +139,10 @@ updateFxSettingLog :: Int -> Double -> Fsd.FxSettingData -> Fsd.FxSettingData ->
 updateFxSettingLog plsf profits fsd fsdf = 
   let fsl = Fsd.fxSettingLog fsd
       fs  = Fsd.fxSetting fsd
-{-      
       fsl' = if M.member fs fsl
              then M.adjust (\(a, b) -> (a + profits, b + 1)) fs fsl
              else M.insert fs (profits, 1) fsl
--}
+{-      
       fsl' = if M.member fs fsl
              then M.filter(\(a, _) -> 0 < a) $ M.adjust (\(a, b) -> (a + profits, b + 1)) fs fsl
              else if 0 < profits
@@ -154,7 +153,8 @@ updateFxSettingLog plsf profits fsd fsdf =
                    L.sortBy (\(_, (a, a')) (_, (b, b')) -> compare (a / fromIntegral a') (b / fromIntegral b')) $
                    M.toList fsl'
               else fsl'
-  in fsd { Fsd.fxSettingLog = unionFxSettingLog fsl'' (Fsd.fxSettingLog fsdf)
+-}
+  in fsd { Fsd.fxSettingLog = unionFxSettingLog fsl' (Fsd.fxSettingLog fsdf)
          }
   
 choice1 :: [Bool] -> Int -> b -> b -> b
