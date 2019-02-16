10f45e791d67f89a048234505c4df36716eea883
MIYATA Tadaaki <andes@flg.jp>
2019-02-16 17:04:20 +0900

    Experiment

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,6 @@ git add . && git commit -m "Experiment" && git push
 oc login -u andesm -p a
 ./scaledown.sh
 oc start-build fx-system-trade
-python get_rate_data/clear_fx-trade.py
 sleep 10
 oc get pods | grep 'fx-system-trade.*Terminating'
 while [ $? = 0 ]
@@ -11,6 +10,7 @@ do
     sleep 5
     oc get pods | grep 'fx-system-trade.*Terminating'
 done
+python get_rate_data/clear_fx-trade.py
 oc get pods | grep 'fx-system-trade.*build.*Running'
 while [ $? = 0 ]
 do
