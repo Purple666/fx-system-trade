684fdddf401d90cb82626490a493a5f9539936d9
MIYATA Tadaaki <andes@flg.jp>
2019-03-03 10:33:02 +0900

    Experiment

--- fx-system-trade/src/FxTrade.hs
+++ fx-system-trade/src/FxTrade.hs
@@ -125,6 +125,12 @@ evaluate ctd fsdi fsd f1 forceSell td =
           evaluateProfitDec fto ftado = (chart, Ftd.Sell)
         | otherwise = (0, Ftd.None)
 
+        | (Ftd.side td == Ftd.None || Ftd.side td == Ftd.Sell) &&
+          evaluateProfitInc fto ftado = (chart, Ftd.Buy)
+        | (Ftd.side td == Ftd.None || Ftd.side td == Ftd.Buy) &&
+          evaluateProfitDec fto ftado = (chart, Ftd.Sell)
+        | otherwise = (0, Ftd.None)
+
 
         | (Ftd.side td == Ftd.None ||
            (Fs.getTradeHoldTime fsd < tradeDate && Ftd.side td == Ftd.Sell)) &&
@@ -138,9 +144,11 @@ evaluate ctd fsdi fsd f1 forceSell td =
         | Ftd.side td == Ftd.None && evaluateProfitDec fto ftado = (chart, Ftd.Sell)
         | otherwise = (0, Ftd.None)
 -}
-        | (Ftd.side td == Ftd.None || Ftd.side td == Ftd.Sell) &&
+        | (Ftd.side td == Ftd.None ||
+           (0 < tradeRate - chart && Ftd.side td == Ftd.Sell)) &&
           evaluateProfitInc fto ftado = (chart, Ftd.Buy)
-        | (Ftd.side td == Ftd.None || Ftd.side td == Ftd.Buy) &&
+        | (Ftd.side td == Ftd.None ||
+           (0 < chart - tradeRate && Ftd.side td == Ftd.Buy)) &&
           evaluateProfitDec fto ftado = (chart, Ftd.Sell)
         | otherwise = (0, Ftd.None)
       (profits, close)
