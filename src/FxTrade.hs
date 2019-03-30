fe784f934c162cf1e54a74005e4e107199757e89
MIYATA Tadaaki <andes@flg.jp>
2019-03-30 18:37:24 +0900

    Experiment

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -138,11 +138,6 @@ evaluate ctd fsdi fsd f1 forceSell td =
           evaluateProfitDec fto ftado = (chart, Ftd.Sell)
         | otherwise = (0, Ftd.None)
 
-        | Ftd.side td == Ftd.None && evaluateProfitInc fto ftado = (chart, Ftd.Buy)
-        | Ftd.side td == Ftd.None && evaluateProfitDec fto ftado = (chart, Ftd.Sell)
-        | otherwise = (0, Ftd.None)   
-
--}
         | (Ftd.side td == Ftd.None ||
            (0 < tradeRate - chart && Ftd.side td == Ftd.Sell)) &&
           evaluateProfitInc fto ftado = (chart, Ftd.Buy)
@@ -150,6 +145,11 @@ evaluate ctd fsdi fsd f1 forceSell td =
            (0 < chart - tradeRate && Ftd.side td == Ftd.Buy)) &&
           evaluateProfitDec fto ftado = (chart, Ftd.Sell)
         | otherwise = (0, Ftd.None)
+
+-}
+        | Ftd.side td == Ftd.None && evaluateProfitInc fto ftado = (chart, Ftd.Buy)
+        | Ftd.side td == Ftd.None && evaluateProfitDec fto ftado = (chart, Ftd.Sell)
+        | otherwise = (0, Ftd.None)   
       (profits, close)
 {-
         | Ftd.side td == Ftd.Buy && (forceSell || Fs.getLearningTestTime fsdi < tradeDate ||
