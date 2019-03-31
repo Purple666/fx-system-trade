b03053b7bc838ae4f309391f76f4a1fe816cb2ab
MIYATA Tadaaki <andes@flg.jp>
2019-03-31 12:26:33 +0900

    Experiment

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -93,9 +93,12 @@ evaluate ctd fsdi fsd f1 forceSell td =
       ftcl      = Fsd.fxTaCloseLoss   $ Fsd.fxSetting fsdi
       lt  = Fs.getLearningTime     fsdi
       ltt = Fs.getLearningTestTime fsdi
+{-      
       lcd = if 60 * 24 * 5 < lt
             then 60 * 24 * 5
             else lt
+-}
+      lcd = 60 * 24 * 5 * 4 * 3
       unrealizedPL
         | Ftd.side td == Ftd.Buy  = Ftd.realizedPL td + 25 * f1 td chart * ((chart / tradeRate) - 1)
         | Ftd.side td == Ftd.Sell = Ftd.realizedPL td + 25 * f1 td chart * (1 - (chart / tradeRate))
