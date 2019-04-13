b821acbcde589cb7689072e9061293cf6667f049
MIYATA Tadaaki <andes@flg.jp>
2019-04-13 13:23:20 +0900

    Experiment

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -403,7 +403,7 @@ backTest log l td fsd xcd =
                              in (fsd2, td2))
                     (fsd, td) ctdl
   in if log
-     then (fsd, td3 { Ftd.chartLength = l })
+     then (Fs.checkAlgoSetting fsd3, td3 { Ftd.chartLength = l })
      else (Fs.checkAlgoSetting fsd3, td3 { Ftd.chartLength = l })
 
 learning :: Fsd.FxSettingData ->
