b97d9a4db94ff7f9ab2f20701a2185aed4a0efd2
MIYATA Tadaaki <andes@flg.jp>
2019-04-28 12:11:23 +0900

    Experiment

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -162,6 +162,7 @@ evaluate ctd fsdi fsd f1 forceSell td =
 -}
         | Ftd.side td == Ftd.None && evaluateProfitInc fto ftado = (chart, Ftd.Buy)
         | Ftd.side td == Ftd.None && evaluateProfitDec fto ftado = (chart, Ftd.Sell)
+        | otherwise = (0, Ftd.None)
       (profits, close)
         | open /= Ftd.None && Ftd.side td == Ftd.Buy  = (chart - tradeRate, Ftd.Close)
         | open /= Ftd.None && Ftd.side td == Ftd.Sell = (tradeRate - chart, Ftd.Close)
