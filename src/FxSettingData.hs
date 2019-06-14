5749f443c54d9c43dc29bea327aae25df19231ce
MIYATA Tadaaki <andes@flg.jp>
2019-06-14 18:18:57 +0900

    Experiment

--- src/FxSettingData.hs
+++ src/FxSettingData.hs
@@ -139,14 +139,14 @@ getLossCutRate fsd =
   let ls = learningSetting $ fxSetting fsd
   in if failProfit ls == 0 || trFail ls == 0
      then -Gsd.initalProperty Gsd.gsd
-     else -(failProfit ls / (fromIntegral $ trFail ls)) * 2
+     else -(failProfit ls / (fromIntegral $ trFail ls)) * getLearningTestTimes2 fsd
 
 getProfitRate :: FxSettingData -> Double
 getProfitRate fsd =
   let ls = learningSetting $ fxSetting fsd
   in if successProfit ls == 0 || trSuccess ls == 0
      then Gsd.initalProperty Gsd.gsd
-     else (successProfit ls / (fromIntegral $ trSuccess ls)) * 2
+     else (successProfit ls / (fromIntegral $ trSuccess ls)) * getLearningTestTimes2 fsd
 
 getLearningTestTime :: FxSettingData -> Int
 getLearningTestTime fsd =
