c1ac28c18d781b4bbcec90412d3daa0b91436d53
MIYATA Tadaaki <andes@flg.jp>
2019-06-15 13:54:17 +0900

    Experiment

--- src/GlobalSettingData.hs
+++ src/GlobalSettingData.hs
@@ -26,8 +26,8 @@ gsd :: GlobalSettingData
 gsd =
   GlobalSettingData { taMargin              = 10
                     , makeTreeCount         = 10
-                    , algorithmRepeat       = 2
-                    , learningTestCount     = 3
+                    , algorithmRepeat       = 10
+                    , learningTestCount     = 10
                     , taOpenAndRate         = 1
                     , taCloseProfitAndRate  = 1
                     , taCloseLossAndRate    = 1
