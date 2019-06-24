f4e354ace75b5b03a681a6622d0162b34460afb0
MIYATA Tadaaki <andes@flg.jp>
2019-06-24 21:52:20 +0900

    Experiment

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 git add . && git commit -m "Experiment" && git push
 oc login -u andesm -p a
+oc get pods | egrep '(fx-system-trade.*build.*Running|fx-system-trade.*build.*Init:[0-9]|fx-system-trade.*build.*PodInitializing)'
 ./scaledown.sh
 oc get pod | grep 'fx-system-trade-.*-build' | awk '{print $1}' | xargs oc delete pod
 oc start-build fx-system-trade
@@ -10,7 +11,6 @@ do
     sleep 5
     oc get pods | grep 'fx-system-trade.*Terminating'
 done
-oc get pods | egrep '(fx-system-trade.*build.*Running|fx-system-trade.*build.*Init:[0-9]|fx-system-trade.*build.*PodInitializing)'
 while [ $? = 0 ]
 do
     sleep 5
