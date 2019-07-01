a14ee7696c98baeaf29326b91796ef387764d144
MIYATA Tadaaki <andes@flg.jp>
2019-07-01 16:46:21 +0900

    Experiment

--- rebuild.sh
+++ rebuild.sh
@@ -5,6 +5,7 @@ oc get pods | egrep '(fx-system-trade.*build.*Running|fx-system-trade.*build.*In
 ./scaledown.sh
 oc get pod | grep 'fx-system-trade-.*-build' | awk '{print $1}' | xargs oc delete pod
 oc start-build fx-system-trade
+sleep 10
 oc get pods | grep 'fx-system-trade.*Terminating'
 while [ $? = 0 ]
 do
