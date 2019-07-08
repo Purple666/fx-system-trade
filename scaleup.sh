084bb3280f29112831bee2a00a1bae6b2fc46de6
MIYATA Tadaaki <andes@flg.jp>
2019-07-08 20:51:04 +0900

    Experiment

--- scaleup.sh
+++ scaleup.sh
@@ -3,4 +3,4 @@
 oc scale deploymentconfig --replicas=1 fx-system-trade-backtest-latest
 #oc scale deploymentconfig --replicas=1 fx-system-trade-backtest-retry
 oc scale deploymentconfig --replicas=1 fx-system-trade-backtest
-oc scale deploymentconfig --replicas=1 fx-system-trade-trade-practice
+#oc scale deploymentconfig --replicas=1 fx-system-trade-trade-practice
