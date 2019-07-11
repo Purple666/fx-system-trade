5ccf0a0e841c50ae7c04cdc69d22c68ada5ceb43
MIYATA Tadaaki <andes@flg.jp>
2019-07-11 21:50:22 +0900

    Experiment

--- src/FxTradeData.hs
+++ src/FxTradeData.hs
@@ -82,12 +82,11 @@ initFxTradeDataCommon =
 
 getEvaluationValue :: FxTradeData -> Double
 getEvaluationValue x =
-  profit x
-{-  
-  profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
   if profit x < 0 && unrealizedPL x < Gsd.initalProperty Gsd.gsd
   then - (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
   else   (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
+--  (profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
+{-  
   unrealizedPL x / (fromIntegral $ chartLength x)
 
 (profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
