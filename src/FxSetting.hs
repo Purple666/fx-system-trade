dad1bc853551025a4688cc42112ed7817a096be4
MIYATA Tadaaki <andes@flg.jp>
2019-07-13 11:41:23 +0900

    Experiment

--- src/FxSetting.hs
+++ src/FxSetting.hs
@@ -138,7 +138,7 @@ createInitialGaData :: MonadRandom m =>
 createInitialGaData n x =
   Ga.learningDataList <$>
   concat <$>
-  mapM (\_ -> mapM createRandomGaData $ Ga.getGaDataList x) [1..2]
+  mapM (\_ -> mapM createRandomGaData $ Ga.getGaDataList x) [1 .. Gsd.learningTestCount Gsd.gsd]
 
 copyFxSettingData :: MonadRandom m =>
                      Ga.LearningData Fsd.FxSettingData ->
