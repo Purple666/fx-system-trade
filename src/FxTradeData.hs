01f879740ab42b0eb6103d7e04b4f6e332870ecc
MIYATA Tadaaki <andes@flg.jp>
2019-07-13 16:15:26 +0900

    Experiment

--- src/FxTradeData.hs
+++ src/FxTradeData.hs
@@ -82,13 +82,15 @@ initFxTradeDataCommon =
 
 getEvaluationValue :: FxTradeData -> Double
 getEvaluationValue x =
-  trSuccess x  * if profit x < 0 && unrealizedPL x < Gsd.initalProperty Gsd.gsd
-                 then - (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
-                 else   (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
+  (trSuccess x * profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
+
 --  
 {-  
+  if profit x < 0 && unrealizedPL x < Gsd.initalProperty Gsd.gsd
+                 then - (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
+                 else   (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
   unrealizedPL x / (fromIntegral $ chartLength x)
-  (profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
+  
 unrealizedPL x / (fromIntegral $ chartLength x)
 (profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
   --(profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
