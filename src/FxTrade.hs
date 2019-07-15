546be2f46f27408e6636b504e7705b08d8f9a4cb
MIYATA Tadaaki <andes@flg.jp>
2019-07-15 18:26:58 +0900

    Experiment

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -86,8 +86,8 @@ evaluate :: Fad.FxChartTaData ->
 evaluate ctd fsd f1 forceSell td =
   let cd        = Fad.taChart ctd
       chart     = Fcd.close cd
-      chartHigh = Fcd.close cd + Gsd.spread Gsd.gsd / 2 -- Fcd.high cd
-      chartLow  = Fcd.close cd - Gsd.spread Gsd.gsd / 2 -- Fcd.low cd
+      chartHigh = Fcd.high cd
+      chartLow  = Fcd.low cd
       tradeRate = Fcd.close $ Ftd.tradeRate td
       tradeDate = Fcd.no cd - (Fcd.no $ Ftd.tradeRate td)
       ftado     = Fad.open        ctd
