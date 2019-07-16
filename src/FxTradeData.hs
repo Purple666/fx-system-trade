1c812d3874ca0d63af244b00d62cbd15a2388d07
MIYATA Tadaaki <andes@flg.jp>
2019-07-16 15:49:40 +0900

    Experiment

--- src/FxTradeData.hs
+++ src/FxTradeData.hs
@@ -83,13 +83,13 @@ initFxTradeDataCommon =
 getEvaluationValue :: FxTradeData -> Double
 getEvaluationValue x =
   if profit x < 0 && unrealizedPL x < Gsd.initalProperty Gsd.gsd
-  then - (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4)
-  else   (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4)
+  then - (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
+  else   (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
 --  
 {-  
   if profit x < 0 && unrealizedPL x < Gsd.initalProperty Gsd.gsd
-  then - (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
-  else   (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4) / (fromIntegral $ chartLength x)
+  then - (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4)
+  else   (profit x * (unrealizedPL x - Gsd.initalProperty Gsd.gsd) * getWinRatePure x ^ 4)
   profit x
   ((fromIntegral $ trSuccess x) * profit x * unrealizedPL x) / (fromIntegral $ chartLength x)
 
