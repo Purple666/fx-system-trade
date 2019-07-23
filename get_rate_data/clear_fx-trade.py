b06b5a4ed5943c0b7df48180566c594b6520a2ac
MIYATA Tadaaki <andes@flg.jp>
2019-07-23 13:45:59 +0900

    Experiment

--- get_rate_data/clear_fx-trade.py
+++ get_rate_data/clear_fx-trade.py
@@ -4,9 +4,9 @@ import time
 client = pymongo.MongoClient('openshift.flg.jp', 30017)
 db = client.fx
 
-db.fsd_backtest.drop()
+#db.fsd_backtest.drop()
+#db.result_backtest.drop()
 db.trade_practice.drop()
 db.trade_practice_weekly.drop()
-db.result_backtest.drop()
 
     
