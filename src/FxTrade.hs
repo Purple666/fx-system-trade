d64bafae790d6d15b04e73b6b830ec9c3c72acf5
MIYATA Tadaaki <andes@flg.jp>
2019-08-17 11:05:17 +0900

    Experiment

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -113,12 +113,12 @@ evaluateOne ctd fsd f1 forceSell td fs =
         | open /= Ftd.None && Ftd.side td == Ftd.Sell = (tradeRate - chart, Ftd.Sell)
         | Ftd.side td == Ftd.Buy &&
           (forceSell || lcd < tradeDate ||
-           (0 < chart - tradeRate && Ta.getPrepareTimeAll fsd < tradeDate && evaluateProfitDec ftcp ftadcp) ||
-           (chart - tradeRate < 0 && Ta.getPrepareTimeAll fsd < tradeDate && evaluateProfitDec ftcl ftadcl)) = (chart - tradeRate, Ftd.Buy)
+           (0 < chart - tradeRate && evaluateProfitDec ftcp ftadcp) ||
+           (chart - tradeRate < 0 && evaluateProfitDec ftcl ftadcl)) = (chart - tradeRate, Ftd.Buy)
         | Ftd.side td == Ftd.Sell &&
           (forceSell || lcd < tradeDate ||
-            (0 < tradeRate - chart && Ta.getPrepareTimeAll fsd < tradeDate && evaluateProfitInc ftcp ftadcp) ||
-            (tradeRate - chart < 0 && Ta.getPrepareTimeAll fsd < tradeDate && evaluateProfitInc ftcl ftadcl)) = (tradeRate - chart, Ftd.Sell)
+            (0 < tradeRate - chart && evaluateProfitInc ftcp ftadcp) ||
+            (tradeRate - chart < 0 && evaluateProfitInc ftcl ftadcl)) = (tradeRate - chart, Ftd.Sell)
         | otherwise = (0, Ftd.None)
       fs' = if close /= Ftd.None
             then let ls  = Fsd.learningSetting fs
