8365e7443e4c039418cda4c8fc5e2d57e0ff432e
MIYATA Tadaaki <andes@flg.jp>
2019-08-18 12:55:35 +0900

    Experiment

--- src/FxTechnicalAnalysis.hs
+++ src/FxTechnicalAnalysis.hs
@@ -26,13 +26,9 @@ import qualified Tree                    as Tr
 getLearningTestTime :: Fsd.FxSettingData -> Int
 getLearningTestTime fsd =
   let fs = Fsd.learningSetting $ Fsd.fxSetting fsd
-      t = Fsd.getLearningTestTimes fsd * if Fsd.numTraderadeDate fs == 0
-                                         then getHoldTime fsd 
-                                         else getHoldTime fsd + (Fsd.totalTradeDate fs `div` Fsd.numTraderadeDate fs)
-  in if Gsd.maxTradeTime Gsd.gsd < t
-     then Gsd.maxTradeTime Gsd.gsd
-     else t
-
+  in Fsd.getLearningTestTimes fsd * if Fsd.numTraderadeDate fs == 0
+                                    then getHoldTime fsd 
+                                    else etHoldTime fsd + (Fsd.totalTradeDate fs `div` Fsd.numTraderadeDate fs)
 
 getHoldTime :: Fsd.FxSettingData -> Int
 getHoldTime fsd = 
