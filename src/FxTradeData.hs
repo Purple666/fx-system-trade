dde8da567c8cb9d68aea0d0a4a019c1ee1ff01c2
MIYATA Tadaaki <andes@flg.jp>
2019-11-13 11:40:56 +0900

    [CHANGE] BB algorithm

--- src/FxTradeData.hs
+++ src/FxTradeData.hs
@@ -82,8 +82,7 @@ initFxTradeDataCommon =
 
 getEvaluationValue :: FxTradeData -> Double
 getEvaluationValue x =
-  profit x
-  -- (profit x * realizedPL x * getWinRatePure x ^ 4) / fromIntegral (chartLength x)
+  (profit x * realizedPL x * getWinRatePure x ^ 4) / fromIntegral (chartLength x)
 
 getEvaluationValueList :: [FxTradeData] -> Double
 getEvaluationValueList tdlt =
