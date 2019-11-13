c309b3a185f619ac2dfa986614d77015b4fb68db
MIYATA Tadaaki <andes@flg.jp>
2019-11-13 13:20:34 +0900

    [CHANGE] BB algorithm

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -28,7 +28,8 @@ import qualified Tree                    as Tr
 evaluationOk :: [Ftd.FxTradeData] -> Bool
 evaluationOk tdlt =
   -- L.and $ L.map (\x -> Gsd.initalProperty Gsd.gsd  < Ftd.realizedPL x) tdlt
-  0 < (sum $ map Ftd.profit tdlt) && Gsd.initalProperty Gsd.gsd * Gsd.learningTestCount Gsd.gsd < (sum $ Ftd.realizedPL tdlt)
+  0 < (sum $ map Ftd.profit tdlt) &&
+  Gsd.initalProperty Gsd.gsd * (fromIntegral $ Gsd.learningTestCount Gsd.gsd) < (sum $ map Ftd.realizedPL tdlt)
   
 getUnitBacktest :: Ftd.FxTradeData -> Double -> Int
 getUnitBacktest td chart = let u = truncate (25 * (Ftd.realizedPL td / Gsd.quantityRate Gsd.gsd) / chart)
