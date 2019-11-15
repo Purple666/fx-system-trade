4cf2b319b05321c379db8e05fbf66ab0bdd294cf
MIYATA Tadaaki <andes@flg.jp>
2019-11-15 14:08:57 +0900

    [CHANGE] BB algorithm

--- src/GlobalSettingData.hs
+++ src/GlobalSettingData.hs
@@ -22,7 +22,7 @@ data GlobalSettingData = GlobalSettingData
 
 gsd :: GlobalSettingData
 gsd =
-  GlobalSettingData { taRandomMargin        = 3
+  GlobalSettingData { taRandomMargin        = 10
                     , taMiddleLongMargin    = 6
                     , makeTreeCount         = 3
                     , algorithmRepeat       = 3
