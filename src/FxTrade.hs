c257e86b63fc9fe65cfd76f480b97f724c15db8b
MIYATA Tadaaki <andes@flg.jp>
2019-11-25 14:57:29 +0900

    [CHANGE] re-learn whenever the system loses a trade

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -263,7 +263,7 @@ backTest :: Int ->
             Fsd.FxSettingData ->
             IO (Fsd.FxSettingData, Ftd.FxTradeData)
 backTest n td fsd = do
-  let ltt = Ta.getLearningTestTime fsd -- * Fsd.getLearningTestCount fsd
+  let ltt = Ta.getLearningTestTime fsd * Fsd.getLearningTestCount fsd
   fc <- Fr.getChartList (n - Ta.getPrepareTimeAll fsd) (Ta.getPrepareTimeAll fsd + ltt)
   let ctdl = makeChart fsd ltt fc
       fs = Fsd.fxSetting fsd
