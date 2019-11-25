9ffd471e9e0c8bb165334c05054ad707706c89c1
MIYATA Tadaaki <andes@flg.jp>
2019-11-25 13:38:07 +0900

    [CHANGE] re-learn whenever the system loses a trade

--- src/FxSetting.hs
+++ src/FxSetting.hs
@@ -35,12 +35,14 @@ instance Ga.Ga Fsd.FxSettingData where
 
 gaLearningDataFromLog :: Int -> Fsd.FxSettingData -> IO (Ga.LearningData Fsd.FxSettingData)
 gaLearningDataFromLog n fsd = do
-  let fsl = if M.member (Fsd.fxSetting fsd) (Fsd.fxSettingLog fsd)
+  let minp = L.minimum . map fst . M.elems $ Fsd.fxSettingLog fsd
+      maxp = L.maximum . map fst . M.elems $ Fsd.fxSettingLog fsd
+      maxc = L.maximum . map snd . M.elems $ Fsd.fxSettingLog fsd
+      fsl = if M.member (Fsd.fxSetting fsd) (Fsd.fxSettingLog fsd)
             then Fsd.fxSettingLog fsd
-            else M.insert (Fsd.fxSetting fsd) (1, 1) $ Fsd.fxSettingLog fsd
-      (mp, _) = L.minimum $ M.elems fsl
+            else M.insert (Fsd.fxSetting fsd) (maxp, maxc) $ Fsd.fxSettingLog fsd
   fsl' <- mapM (\(fs, (p, c)) -> do (ltt, fc) <- Ft.getChart n 1 fsd { Fsd.fxSetting = fs }
-                                    let fs' = fs { Fsd.learningSetting = (Fsd.learningSetting fs) { Fsd.logProfit = p + mp
+                                    let fs' = fs { Fsd.learningSetting = (Fsd.learningSetting fs) { Fsd.logProfit = p + minp
                                                                                                   , Fsd.logCount  = c
                                                                                                   }
                                                  }
