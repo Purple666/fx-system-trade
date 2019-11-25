727f547267442044fb5e77a47681ad4747e0795f
MIYATA Tadaaki <andes@flg.jp>
2019-11-25 12:09:26 +0900

    [CHANGE] re-learn whenever the system loses a trade

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -39,7 +39,7 @@ getEvaluationValue fsd td =
 
 evaluationOk :: Ftd.FxTradeData -> Fsd.FxSettingData -> Bool
 evaluationOk td fsd =
-  0 < getEvaluationValue fsd td &&
+  Fsd.getLearningTestTimes fsd < Ftd.profit td &&
   50 < Ftd.getWinRate td &&
   Gsd.initalProperty Gsd.gsd < Ftd.realizedPL td
   
