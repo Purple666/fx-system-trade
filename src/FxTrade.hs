777be5cc59fc5367481346e41a11afeab48d848f
MIYATA Tadaaki <andes@flg.jp>
2019-11-26 16:48:45 +0900

    [CHANGE] re-learn whenever the system loses a trade

--- src/FxTrade.hs
+++ src/FxTrade.hs
@@ -37,10 +37,7 @@ getGaEvaluationValue fsd td =
 evaluationOk :: Ftd.FxTradeData -> Fsd.FxSettingData -> Bool
 evaluationOk td fsd =
   0 < getEvaluationValue fsd td &&
-  3 < Ftd.profit td &&
-  0 <= (Ftd.profit . Fsd.resultFxTradeData $ Fsd.fxSettingChart fsd) &&
-  50 < Ftd.getWinRate td &&
-  Gsd.initalProperty Gsd.gsd < Ftd.realizedPL td
+  (fromIntegral $ Fsd.getLearningTestTimes fsd) * (Ftd.profit . Fsd.resultFxTradeData $ Fsd.fxSettingChart fsd) < Ftd.profit td
   
 getUnitBacktest :: Ftd.FxTradeData -> Double -> Int
 getUnitBacktest td chart = let u = truncate (25 * (Ftd.realizedPL td / Gsd.quantityRate Gsd.gsd) / chart)
