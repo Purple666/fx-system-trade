d31751bbc18974b8e74cd5fa5ff65ccdf0d6c66d
MIYATA Tadaaki <andes@flg.jp>
2019-11-28 20:04:25 +0900

    [CHANGE] re-learn whenever the system loses a trade

--- src/GlobalSettingData.hs
+++ src/GlobalSettingData.hs
@@ -23,7 +23,7 @@ data GlobalSettingData = GlobalSettingData
 
 gsd :: GlobalSettingData
 gsd =
-  GlobalSettingData { taRandomMargin        = 3
+  GlobalSettingData { taRandomMargin        = 10
                     , taMiddleLongMargin    = 5
                     , makeTreeCount         = 3
                     , algorithmRepeat       = 3
@@ -31,8 +31,8 @@ gsd =
                     , quantityRate          = 5
                     , treeAndRate           = 1
                     , treeOrRate            = 1
-                    , learningTestTimes     = 5
-                    , learningTestCount     = 5
+                    , learningTestTimes     = 10
+                    , learningTestCount     = 10
                     , fxSettingLogNum       = 30
                     , gaNum                 = 30
                     , maxTradeTime          = 24 * 60 * 5 
