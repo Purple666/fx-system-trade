c666bfb9e16bcfafc852bca84d4f805eef4773f3
MIYATA Tadaaki <andes@flg.jp>
2020-02-20 19:26:19 +0900

    [CHANGE]

--- src/FxTechnicalAnalysis.hs
+++ src/FxTechnicalAnalysis.hs
@@ -27,7 +27,7 @@ import qualified FxTradeData             as Ftd
 getLearningTestTime :: Fsd.FxSettingData -> Int
 getLearningTestTime fsd =
   let ls = Fsd.learningSetting $ Fsd.fxSetting fsd
-  in {- Gsd.learningTestTimes Gsd.gsd * -} 
+  in Gsd.learningTestTimes Gsd.gsd * 
      if Fsd.numTraderadeDate ls == 0
      then getHoldTime fsd + 60
      else getHoldTime fsd + Fsd.totalTradeDate ls `div` Fsd.numTraderadeDate ls
