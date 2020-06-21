89d25217d5e373af6804e5a22fb882718af90133
MIYATA Tadaaki <andes@flg.jp>
2020-06-21 17:37:44 +0900

    [CHANGE]

--- src/FxTradeData.hs
+++ src/FxTradeData.hs
@@ -14,8 +14,8 @@ import qualified FxTechnicalAnalysisData as Fad
 import qualified GlobalSettingData       as Gsd
 
 data FxTradeData =
-  FxTradeData { chart            :: !Fcd.FxChartData
-              , tradeRate        :: !Fcd.FxChartData
+  FxTradeData { chart            :: Fcd.FxChartData
+              , tradeRate        :: Fcd.FxChartData
               , unit             :: !Int
               , side             :: FxSide
               , trSuccess        :: !Int
