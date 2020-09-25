71ec8df2a5a05e61d267fc864d7713219b1fe1f6
MIYATA Tadaaki <andes@flg.jp>
2020-09-25 17:22:54 +0900

    [CHANGE]

--- src/GlobalSettingData.hs
+++ src/GlobalSettingData.hs
@@ -23,15 +23,15 @@ data GlobalSettingData = GlobalSettingData
 
 gsd :: GlobalSettingData
 gsd =
-  GlobalSettingData { taRandomMargin        = 3
-                    , taMiddleLongMargin    = 3
-                    , makeTreeCount         = 3
-                    , algorithmRepeat       = 3
+  GlobalSettingData { taRandomMargin        = 5
+                    , taMiddleLongMargin    = 5
+                    , makeTreeCount         = 5
+                    , algorithmRepeat       = 5
                     , countUpList           = 2
                     , treeAndRate           = 1
                     , treeOrRate            = 1
                     , learningTestTimes     = 5
-                    , learningTestCount     = 50
+                    , learningTestCount     = 25
                     , gaNum                 = 20
                     , fxSettingLogNum       = 50
                     , quantityRate          = 5
